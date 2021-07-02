a553255ce9ae27c34db049aa2d510142a081e327
AnnaBiro <81937689+AnnaBiro@users.noreply.github.com>
2021-07-02 10:56:20 -0400

    Styling footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export default function Footer(){
-  return (<footer>Coded by Anna Biro.<a href="https://github.com/AnnaBiro/Weather-React">Open source code</a>
-    </footer>)
+  return (<small>Coded by Anna Biro.<a href="https://github.com/AnnaBiro/Weather-React">Open source code</a>
+    </small>)
 }
