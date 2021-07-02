15b77ae789ff0499079ad09e871a03c210b56432
AnnaBiro <81937689+AnnaBiro@users.noreply.github.com>
2021-07-02 10:36:42 -0400

    created a footer component

---
+++ src/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Footer(){
+  return (<footer>Coded by Anna Biro.<a href="https://github.com/AnnaBiro/Weather-React">Open source code</a>
+    </footer>)
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Weather />
+      <Footer />
     </div>
   );
 }
